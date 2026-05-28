b9913da56ffe76b3f71c27cde16e4469822c64f8
VineelJoshua-git <planet.vinjosh@gmail.com>
2026-05-28 17:06:09 +0000

    Improve Alert component with dismiss functionality

--- src/common/components/Alert.jsx
+++ src/common/components/Alert.jsx
@@ -1,15 +1,32 @@
-const Alert = ({ alertClassName, children, icon }) => {
+const Alert = ({ alertClassName, children, icon, onClose }) => {
   return (
-    <div className={`alert d-flex flex-row ${alertClassName ? alertClassName : ""}`} role="alert">
+    <div
+      className={`alert d-flex flex-row ${alertClassName ? alertClassName : ""}`}
+      role="alert"
+    >
       {
         icon &&
-        <div className="me-2"><img src={icon} alt=""></img></div>
+        <div className="me-2">
+          <img src={icon} alt="" />
+        </div>
       }
-      <div class="d-flex flex-grow-1">
+
+      <div className="d-flex flex-grow-1">
         {children}
       </div>
+
+      {
+        onClose && (
+          <button
+            type="button"
+            className="btn-close"
+            aria-label="Close alert"
+            onClick={onClose}
+          />
+        )
+      }
     </div>
-  )
+  );
 };
 
 export default Alert;
